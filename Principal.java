4ddfa936e45c5d26ec778327cf923bf72c29bbd6
Adrian <1707621@alu.murciaeduca.es>
2024-02-06 12:02:54 +0100

    Añadido el metodo fechaHora y actualizado el README

--- Principal.java
+++ Principal.java
@@ -1,5 +1,16 @@
-public class Principal {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Arrays {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+        	fechaHora();
 	}
+
+	public static void fechaHora() {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        	Date fechaHora = new Date();
+        	System.out.println("Fecha y hora actual: " + dateFormat.format(fechaHora));
+    }	
+
 }
